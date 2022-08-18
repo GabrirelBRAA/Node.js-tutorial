10fd48ee2cc08dc2921e6829a2fde55192204404
Gabriel <jgbiel29@gmail.com>
2022-08-18 13:24:47 -0300

    Logger exercise

--- myApp.js
+++ myApp.js
@@ -1,5 +1,40 @@
 let express = require('express');
 let app = express();
+require("dotenv").config()
+
+
+
+app.use(function Meme3  (req, res, next){
+    let method = req.method;
+    let ip = req.ip;
+    let path = req.path;
+    console.log(method + " " + path + " - " + ip);
+    next();
+
+})
+
+
+
+let public_path = __dirname + "/public";
+app.use("/public", express.static(public_path))
+
+function Meme(req, res){
+    let Abs_path = __dirname + "/views/index.html"
+res.sendFile(Abs_path);
+}
+function Meme2(req, res){
+     if (process.env.MESSAGE_STYLE == "uppercase"){
+        res.json({"message" : "HELLO JSON"});
+     } else {
+        res.json({"message" : "Hello json"});
+     }
+}
+
+
+app.get("/",Meme)
+app.get("/json", Meme2)
+
+
 
 
 
